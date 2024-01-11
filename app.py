f685ab8cb26ccc1a1807af30e8182ad3a1840a28
Wahyu Enggar Wicaksono <94543772+wahyuenggar@users.noreply.github.com>
2024-01-11 20:27:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_migrate import Migrate
 from models.modelTables import db
 from routes.perawats_bp import perawats_bp
+from routes.pasiens_bp import pasiens_bp
+from routes.dokters_bp import dokters_bp 
 
 app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "Masuk Ke /perawat"
+    return """
+    Masuk Ke /perawat
+    Masuk ke /dokter
+    Masuk ke /pasiien"""
 
 app.config.from_object('config')
 
@@ -15,6 +20,8 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 app.register_blueprint(perawats_bp, url_prefix='/perawat')
+app.register_blueprint(pasiens_bp, url_prefix='/pasien')
+app.register_blueprint(dokters_bp, url_prefix='/dokter')
 
-if __name__ == '__main__':
+if __name__ == '__main__':                  
     app.run(port=8000, debug=True)
